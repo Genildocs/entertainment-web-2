e9a975f4e79aa32143f2e59a26d5e4f802806f82
genildocs <genildocs@gmail.com>
2024-11-01 12:05:13 -0300

    commit

--- app/api/trendings/route.js
+++ app/api/trendings/route.js
@@ -1,12 +1,12 @@
 import { NextResponse } from 'next/server';
 
 export async function GET(request) {
+  const tokenTmb = process.env.NEXT_PUBLIC_TMDB_API_KEY;
   const options = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization:
-        'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1OGQ0MTI2NDI0ODRjNTVhYTM0MmUyNzFhZTU0ZmMxMiIsIm5iZiI6MTczMDQyMTg5Ni4yOTUyODg4LCJzdWIiOiI2NWQ2ZWE5Zjk5NzRlZTAxN2IwNjQwNTIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.OatpsyMSNqysZJGgdVeCe4b3mksshaWcX8i5h17xrtA',
+      Authorization: `Bearer ${tokenTmb}`,
     },
   };
 
